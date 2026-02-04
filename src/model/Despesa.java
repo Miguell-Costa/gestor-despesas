4b7decde1405f821dffb044f632fa6a87fbefa47
Miguell-Costa <jmmc436@gmail.com>
2026-02-04 12:45:03 +0000

    correção base dados e Despesa.java

--- src/model/Despesa.java
+++ src/model/Despesa.java
@@ -5,19 +5,25 @@ package model;
 public class Despesa {
     private int id;
     private String nome;
-    private float valor;
+    private double valor;
+    private int mes;
+    private int ano;
 
     // construtor com id
-    public Despesa(int id, String nome, float valor) {
+    public Despesa(int id, String nome, double valor, int mes, int ano) {
         this.id = id;
         this.nome = nome;
         this.valor = valor;
+        this.mes = mes;
+        this.ano = ano;
     }
 
     // contrutor sem id
-    public Despesa(String nome, float valor) {
+    public Despesa(String nome, double valor, int mes, int ano) {
         this.nome = nome;
         this.valor = valor;
+        this.mes = mes;
+        this.ano = ano;
     }
 
     // getters e setters
@@ -37,12 +43,28 @@ public class Despesa {
         this.nome = nome;
     }
 
-    public float getValor() {
+    public double getValor() {
         return valor;
     }
 
     public void setValor(float valor) {
         this.valor = valor;
+    }
+
+    public int getMes() {
+        return mes;
+    }
+
+    public void setMes(int mes) {
+        this.mes = mes;
+    }
+
+    public int getAno() {
+        return ano;
+    }
+
+    public void setAno(int ano) {
+        this.ano = ano;
     }
 
     @Override
@@ -51,6 +73,8 @@ public class Despesa {
                 "id=" + id +
                 ", nome='" + nome + '\'' +
                 ", valor=" + valor +
+                ", mes=" + mes +
+                ", ano=" + ano +
                 '}';
     }
 }
